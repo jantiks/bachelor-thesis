50a31a68a52ad91d4a21ce899dcf51fb1fc7fcda
jantiks <tigran.arsenyan.2018@gmail.com>
2025-02-22 18:58:56 +0100

    2-connected counter-example proof

--- minorGenerator/find_minor_lib_usage.py
+++ minorGenerator/find_minor_lib_usage.py
@@ -16,6 +16,67 @@ import copy
 import random
 
 
+def generate_graphs_base_plus_two_edges(G):
+    # Get the list of vertices from the base graph
+    vertices = list(G.nodes())
+    
+    # Compute all possible edges in a complete graph on these vertices.
+    all_possible_edges = list(itertools.combinations(vertices, 2))
+    
+    # The base graph already has some edges; compute the missing ones.
+    # (Since the graph is undirected, the tuple order doesn't matter.)
+    base_edges = set(G.edges())
+    missing_edges = [e for e in all_possible_edges if e not in base_edges and (e[1], e[0]) not in base_edges]
+    
+    print("Total missing edges:", len(missing_edges))
+    
+    # Generate new graphs by adding two edges from the missing set to the base graph.
+    graphs = []
+    for extra_edges in itertools.combinations(missing_edges, 4):
+        # Create a copy of the base graph
+        G_new = G.copy()
+        G_new.add_edges_from(extra_edges)
+        graphs.append(G_new)
+    
+    print("Total graphs generated (base + 2 extra edges):", len(graphs))
+    return graphs
+
+def get_all_graphs_K7_minus_one_edge():
+    V = list(range(7))  # vertices 0 through 6
+    all_edges = list(itertools.combinations(V, 2))  # all 21 edges of K7
+    print("Total number of edges in K7:", len(all_edges))
+    
+    graphs = []
+    # For each edge, remove it from the complete graph
+    for missing_edge in all_edges:
+        G = nx.Graph()
+        G.add_nodes_from(V)
+        # Add all edges except the one being removed
+        edges = [e for e in all_edges if e != missing_edge]
+        G.add_edges_from(edges)
+        graphs.append(G)
+    
+    print("Total number of graphs generated (K7 minus one edge):", len(graphs))
+    return graphs
+
+def get_all_graphs_K7_minus_two_edges():
+    V = list(range(7))  # vertices 0 through 6
+    all_edges = list(itertools.combinations(V, 2))  # all 21 edges of K7
+    print("Total number of edges in K7:", len(all_edges))
+    
+    graphs = []
+    # Iterate over all possible pairs of edges to remove
+    for missing_edges in itertools.combinations(all_edges, 1):
+        G = nx.Graph()
+        G.add_nodes_from(V)
+        # Add all edges except the two that are being removed
+        edges = [e for e in all_edges if e not in missing_edges]
+        G.add_edges_from(edges)
+        graphs.append(G)
+    
+    print("Total number of graphs generated (K7 minus two edges):", len(graphs))
+    return graphs
+
 def get_all_graphs_H(n):
     V = [i for i in range(n)]
     E_C6 = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (5, 0)]
@@ -94,10 +155,9 @@ def exists_kempe(G, start, end, original_begining, looked_at=set()):
             return True
     return False
 
-def build_kempe_chain(g1, begining, end, set_of_all_g1, original_begining):
+def build_kempe_chain(g1, begining, end, set_of_all_g1, original_begining, level=0):
     if exists_kempe(g1, begining, end, original_begining):
         return
-
     begining_neighbours = g1[begining]
 
     already_built = False
@@ -108,29 +168,20 @@ def build_kempe_chain(g1, begining, end, set_of_all_g1, original_begining):
             continue
 
     
-        if (color(begining) == color(end) and color(vertex) == color(original_begining)):
+        if (color(begining) == color(end) and color(vertex) == color(original_begining)) or (color(begining) == color(original_begining) and color(vertex) == color(end)):
+            # print('begining: ', begining, 'vertex: ', vertex, 'end: ', end, 'original_begining: ', original_begining)
             if already_built:
                 newG = copy.deepcopy(g1)
                 newG[begining].add(vertex)
                 newG[vertex].add(begining)
+                # showGraph(getNetworkxGraph(newG))
                 set_of_all_g1.append(newG)
-                build_kempe_chain(newG, vertex, end, set_of_all_g1, original_begining)
+                build_kempe_chain(newG, vertex, end, set_of_all_g1, original_begining, level=level+1)
             else:
                 g1[begining].add(vertex)
                 g1[vertex].add(begining)
-                build_kempe_chain(g1, vertex, end, set_of_all_g1, original_begining)
-                already_built = True
-        elif (color(begining) == color(original_begining) and color(vertex) == color(end)):
-            if already_built:
-                newG = copy.deepcopy(g1)
-                newG[begining].add(vertex)
-                newG[vertex].add(begining)
-                set_of_all_g1.append(newG)
-                build_kempe_chain(newG, vertex, end, set_of_all_g1, original_begining)
-            else:
-                g1[begining].add(vertex)
-                g1[vertex].add(begining)
-                build_kempe_chain(g1, vertex, end, set_of_all_g1, original_begining)
+                # showGraph(getNetworkxGraph(g1))
+                build_kempe_chain(g1, vertex, end, set_of_all_g1, original_begining, level=level+1)
                 already_built = True
 
 def get_edges_from_adjacency_list(adjacency_list):
@@ -141,58 +192,42 @@ def get_edges_from_adjacency_list(adjacency_list):
             edges.add(tuple(sorted((node, neighbor))))
     return list(edges)  
 
+def showGraph(G):
+    plt.figure(figsize=(6, 6))
+    nx.draw(G, with_labels=True, node_color='lightblue', node_size=500, font_size=15, font_color='black')
+    print(G)
+    plt.title("Graph G: Cycle of 10 Nodes")
+    plt.show()
+
+def getNetworkxGraph(dict):
+    G = nx.Graph()
+
+    # Add edges to the graph by iterating over the dictionary
+    for node, neighbors in dict.items():
+        for neighbor in neighbors:
+            G.add_edge(node, neighbor)
+    
+    return G
+
 if __name__ == "__main__":
-    # big = nx.Graph([('0b', '1b'), ('1b', '2b'), ('2b', '3b'), ('3b', '0b'), ('2b', '0b'), ('1b', '3b'), ('3b', '230d')])
-    # small = nx.Graph([('0b', '1b'), ('1b', '2b'), ('2b', '3b'), ('3b', '0b')])
-
-
-    # isomatcher = nx.isomorphism.GraphMatcher(big, small)
-    # if isomatcher.subgraph_is_isomorphic():
-    #     print("Isomorphic")
-    # else:
-    #     print("NON-Isomorphic")
-    # mode = sys.argv[1] if len(sys.argv) > 1 else "automated"
-    # main(mode)
-
-    # small = [('0a', '1a'), ('1a', '2a'), ('2a', '3a'), ('3a', '4a'), ('4a', '5a'), ('5a', '0a'), ('1a', '4a')]
-    small = [('a1', 'b1'), ('b1', 'c1'), ('c1', 'd1'), ('d1', 'e1'), ('e1', 'f1'), ('a1', 'f1'), ('b1', 'f1'), ('a1', 'd1')]
-
-    big = [('a1', 'b3'), ('b3', 'a3'), ('a3', 'b1'), # a1-b1 kempe chain
-           ('f1', 'a2'), ('a2', 'f2'), ('f2', 'a1'), # a1-f1 kempe chain
-           ('f1', 'b2'), ('b2', 'f2'), ('f2', 'b3'), # b1-f1 kempe chain
-           ('f1', 'e2'), ('e2', 'f2'), ('f2', 'e1'), # e1-f1 kempe chain
-        #    ('f3', 'd1'), ('f3', 'd2'), ('d2', 'f1'), # d1-f1 kempe chain
-           ('e1', 'd2'), ('d2', 'e2'), ('e2', 'd1'), # d1-e1 kempe chain
-           ('c1', 'd2'), ('d2', 'c2'), ('c2', 'd1'), # c1-d1 kempe chain
-           ('c1', 'b3'), ('b3', 'c2'), ('c2', 'b1'), # c1-b1 kempe chain
-           ('d1', 'a2'), ('a2', 'd3'), ('d3', 'a1'), # a1-d1 kempe chain
-        #    ('e1', 'b3'), ('b3', 'e2'), ('e2', 'b1'), # b1-e1 kempe chain
-        #    ('e1', 'a2'), ('a2', 'e2'), ('e2', 'a1'), # a1-e1 kempe chain
-        #    ('e1', 'c3'), ('c3', 'e2'), ('e2', 'c1'), # c1-e1 kempe chain
-        #    ('d1', 'b2'), ('b2', 'd2'), ('d2', 'b1'), # b1-d1 kempe chain
-           ]
-
-    # tSmall = nx.Graph(small)
-    # big = zg(tSmall).edges()
-
-    # Find an assignment of sets of square variables to the triangle variables
-    embedding = find_embedding(small, big, random_seed=10, suspend_chains={"a1": [['a1']], "b1": [["b1"]], "c1": [["c1"]], "d1": [["d1"]], "e1": [["e1"]], "f1": [["f1"]]})
-    print(embedding)
+    noMinorMinimalGraph = None
     terminate = False
-    graphs = get_all_graphs_H(6)
+    graphs = get_all_graphs_K7_minus_one_edge()
+    print(len(graphs), graphs)
     index = 0
-    print("FAILING MINOR", graphs[46].edges())
-    graphs = [graphs[47]]
+    foundedMinor = 0
+    noMinor = 0
+    # graphs = generate_graphs_base_plus_two_edges(nx.Graph([('0', '1'), ('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('5', '0'), ('0', '6'), ('6', '3')]))
+    # graphs = [nx.Graph([('0', '1'), ('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('5', '0'), ('0', '6'), ('6', '3')])]
     for minor in graphs:
         if terminate:
             print("Terminating found minor")
             break
-        n = 16
+        n = 15
         H = {
             f"{node}a": set([f"{neighbor}a" for neighbor in neighbors])
             for node, neighbors in minor.adj.items()
         }
-
         # Create a mapping from node to index
 
         # Initialize the adjacency matrix
@@ -208,7 +243,7 @@ if __name__ == "__main__":
 
 
         all_g = []
-        comb_size = n % 12
+        comb_size = n % 14
         if (comb_size != 0):
             all_combs = list(combinations_with_replacement(H.keys(), comb_size))
             for comb in all_combs:
@@ -221,62 +256,56 @@ if __name__ == "__main__":
 
         # final_results = []
         
+        all_g_index = 0
         for g1 in all_g:
+            all_g_index += 1
             set_of_all_g1 = [copy.deepcopy(g1)]
             for begining in H: 
                 for end in H[begining]:
                     for graph in set_of_all_g1:
                         build_kempe_chain(graph, begining, end, set_of_all_g1, begining)
             
+            minorCheckingIndex = 0
             for kempe_graph in set_of_all_g1:
+                print("KEMPE GRAPH", minorCheckingIndex, index, all_g_index, len(all_g), len(set_of_all_g1))
+                # showGraph(getNetworkxGraph(kempe_graph))
+                minorCheckingIndex += 1
                 big = get_edges_from_adjacency_list(kempe_graph)
-                minor_embd = find_embedding(small, big, random_seed=10, suspend_chains={"0a": [['0a']], "1a": [["1a"]], "2a": [["2a"]], "3a": [["3a"]], "4a": [["4a"]], "5a": [["5a"]]})
+                minor_embd = find_embedding(small, big, random_seed=10, suspend_chains={"0a": [['0a']], "1a": [["1a"]], "2a": [["2a"]], "3a": [["3a"]], "4a": [["4a"]], "5a": [["5a"]], "6a": [["6a"]]})
                 if minor_embd:
+                    foundedMinor += 1
                     pass
-                    # print("Found minor")
                 else:
                     no_minor = True
                     for i in range(10):
-                        minor_embd = find_embedding(small, big, random_seed=random.randint(1, 1000), suspend_chains={"0a": [['0a']], "1a": [["1a"]], "2a": [["2a"]], "3a": [["3a"]], "4a": [["4a"]], "5a": [["5a"]]})
+                        minor_embd = find_embedding(small, big, random_seed=random.randint(1, 1000), suspend_chains={"0a": [['0a']], "1a": [["1a"]], "2a": [["2a"]], "3a": [["3a"]], "4a": [["4a"]], "5a": [["5a"]], "6a": [["6a"]]})
+
                         if minor_embd:
                             no_minor = False
                             
                     
                     if no_minor:
+                        if noMinorMinimalGraph is None:
+                            noMinorMinimalGraph = kempe_graph
+                            noMinorGraph = minor
+                        elif len(get_edges_from_adjacency_list(kempe_graph)) < len(get_edges_from_adjacency_list(noMinorMinimalGraph)):
+                            noMinorMinimalGraph = kempe_graph
+                            noMinorGraph = minor
                         print("No minor found")
                         print(small)
                         print(kempe_graph)
                         print("No minor")
                         terminate = True
+                        noMinor += 1
                         break
             # final_results.extend(set_of_all_g1)
             set_of_all_g1 = []
         index += 1
         print("NEXT STEP", index, len(graphs))
 
-            
-
-        # print("ASD SETOF ALL G", len(final_results), len(all_g))
-        # small = get_edges_from_adjacency_list(H)
-        # for graph in final_results:
-        #     big = get_edges_from_adjacency_list(graph)
-        #     minor_embd = find_embedding(small, big, random_seed=10, suspend_chains={"0a": [['0a']], "1a": [["1a"]], "2a": [["2a"]], "3a": [["3a"]], "4a": [["4a"]], "5a": [["5a"]]})
-        #     if minor_embd:
-        #         pass
-        #         # print("Found minor")
-        #     else:
-        #         no_minor = True
-        #         for i in range(10):
-        #             minor_embd = find_embedding(small, big, random_seed=random.randint(1, 1000), suspend_chains={"0a": [['0a']], "1a": [["1a"]], "2a": [["2a"]], "3a": [["3a"]], "4a": [["4a"]], "5a": [["5a"]]})
-        #             if minor_embd:
-        #                 no_minor = False
-                        
-                
-        #         if no_minor:
-        #             print("No minor found")
-        #             print(small)
-        #             print(graph)
-        #             print("No minor")
-        #             terminate = True
-        #             break
+    print(noMinorGraph.edges())
+    print(noMinorMinimalGraph)
+    showGraph(noMinorGraph)
+    showGraph(getNetworkxGraph(noMinorMinimalGraph))
+    print("ASD STATISTICS", foundedMinor, noMinor)
     
