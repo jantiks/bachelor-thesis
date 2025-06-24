bbd4bcfca19937c51e56b71a4497328fc0f6cbab
jantiks <tigran.arsenyan.2018@gmail.com>
2025-06-24 14:40:17 +0200

    algorithm fixes, chapter 4, abstract, conclusion

--- minorGenerator/find_minor_lib_usage.py
+++ minorGenerator/find_minor_lib_usage.py
@@ -3,18 +3,13 @@ import itertools
 from copy import deepcopy
 from minorminer import find_embedding
 import matplotlib.pyplot as plt
-import sys
-import resource, sys
 import random
 import numpy as np
 import igraph as ig
-from collections import defaultdict
-import logging
-import math
 from itertools import combinations_with_replacement
 import copy
 import random
-import multiprocessing
+import subprocess
 
 
 def even_weight_bitstrings(n):
@@ -159,6 +154,13 @@ def getNetworkxGraph(dict):
 
     return G
 
+def get_graphs_from_geng(n):
+    proc = subprocess.run(['geng', str(n), "-c", "-d2"], capture_output=True, text=True)
+    graphs = []
+    for line in proc.stdout.strip().splitlines():
+        G = nx.from_graph6_bytes(line.encode())
+        graphs.append(G)
+    return graphs
 
 def get_all_graphs_H(n):
     V = [i for i in range(n)]
@@ -220,73 +222,8 @@ def testIfMinorExists(minor, parent, suspend_chains):
 
     return False
 
-
-def build(
-    G, chains_to_build, current, end, visited, available_vertices, minor, suspend_chains
-):
-    if current == end:
-        if not chains_to_build:
-            if testIfMinorExists(
-                minor, get_edges_from_adjacency_list(G), suspend_chains
-            ):
-                return
-            else:
-                if not testIfMinorExists(
-                    minor, get_edges_from_adjacency_list(G), suspend_chains
-                ):
-                    print("FOUND COUNTEREXAMPLE")
-                    print(G)
-                    print(minor)
-                    print(suspend_chains)
-                    input("WAIT")
-            return
-
-        next_start, next_end = chains_to_build[0]
-        remaining_chains = chains_to_build[1:]
-
-        color_to_look_for = color(next_end)
-        next_available = [v for v in G.keys() if color(v) == color_to_look_for]
-
-        build(
-            G,
-            remaining_chains,
-            next_start,
-            next_end,
-            {next_start},
-            next_available,
-            minor,
-            suspend_chains,
-        )
-        return
-
-    # Process current chain
-    for vertex in available_vertices:
-        newG = copy.deepcopy(G)
-        newG[current].add(vertex)
-        newG[vertex].add(current)
-
-        color_to_look_for = (
-            color(current) if color(vertex) == color(end) else color(end)
-        )
-        next_available = [
-            v
-            for v in G.keys()
-            if v not in visited.union({vertex}) and color(v) == color_to_look_for
-        ]
-
-        build(
-            newG,
-            chains_to_build,
-            vertex,
-            end,
-            visited.union({vertex}),
-            next_available,
-            minor,
-            suspend_chains,
-        )
-
-
-if __name__ == "__main__":
+## This function is helper function to manually validate again the possible counter examples the program found
+def validate_result():
     counter_example = {
         "0a": {"1a", "3a", "4b", "5a", "2a"},
         "1a": {"3b", "0a", "4b", "5a", "2a"},
@@ -337,78 +274,94 @@ if __name__ == "__main__":
         if hamming_distance(u, v) == 2:
             G.add_edge(u, v)
     minor = nx.complete_graph(5)
-    print("ASD", G.edges())
-    if testIfMinorExists(minor.edges(), G.edges(), suspend_chains):
-        print("FASLE CALL")
-    else:
-        print("HAPPPY")
+    for i in range(100):
+        if testIfMinorExists(minor.edges(), G.edges(), suspend_chains):
+            print("FASLE CALL")
+            return
 
-    input("WOW")
+    input("With probability almost 1, this is a counterexample.")
 
-    n = 14  ## build graphs on n vertices
-    noMinorMinimalGraph = None
-    noMinorGraph = None
-    terminate = False
-    nominors = []
-    # graphs =
-    # graphs = get_all_graphs_K7_minus_one_edge()
-    # graphs = generate_graphs_base_plus_two_edges(nx.Graph([('0', '1'), ('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('5', '0'), ('0', '6'), ('6', '3')]))  ## counterexample from paper + 2edges
-    # graphs = [
-    #     nx.Graph(
-    #         [
-    #             ("0", "1"),
-    #             ("1", "2"),
-    #             ("2", "3"),
-    #             ("3", "4"),
-    #             ("4", "5"),
-    #             ("5", "0"),
-    #             ("0", "6"),
-    #             ("6", "3"),
-    #         ]
-    #     )
-    # ]  ## counterexample from paper
-    # graphs = [nx.Graph([('0', '1'), ('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('5', '0'), ('0', '3'), ('0', '4'), ('1', '4')])]
-    num_vertices_of_minor = 6
-    graphs = get_all_graphs_H(num_vertices_of_minor)
-    # graphs = generate_graphs_base_plus_edges(nx.Graph([('0', '1'), ('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('5', '6'), ('6', '0')]), 1) ## counter example
-    # graphs = [nx.Graph([('0', '1'), ('0', '2'), ('0', '5'), ('1', '4'), ('1', '6'), ('2', '3'), ('3', '4'), ('6', '4'), ('6', '5'), ('5', '2')])] ## counter example 2
-    # graphs = [nx.Graph([('0', '1'), ('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('5', '0'), ('2', '5')])] ## counter example
-    random.shuffle(graphs)
+def build(
+    G, chains_to_build, current, end, visited, available_vertices, minor, suspend_chains
+):
+    if current == end:
+        if not chains_to_build:
+            if testIfMinorExists(
+                minor, get_edges_from_adjacency_list(G), suspend_chains
+            ):
+                return
+            else:
+                if not testIfMinorExists(
+                    minor, get_edges_from_adjacency_list(G), suspend_chains
+                ):
+                    print("FOUND COUNTEREXAMPLE")
+                    print("Big graph:", G)
+                    print("Minor graph:", minor)
+                    input("WAIT: FOUND COUNTEREXAMPLE")
+            return
 
-    index = 0
-    foundedMinor = 0
-    noMinor = 0
+        next_start, next_end = chains_to_build[0]
+        remaining_chains = chains_to_build[1:]
 
-    for minor in graphs:
-        if terminate:
-            print("Terminating early found minor")
-            break
-        # builidng the graph H in the format suitable for the minorminer
-        H = {
-            f"{node}a": set([f"{neighbor}a" for neighbor in neighbors])
-            for node, neighbors in minor.adj.items()
-        }
+        color_to_look_for = color(next_end)
+        next_available = [v for v in G.keys() if color(v) == color_to_look_for]
 
-        small = get_edges_from_adjacency_list(H)
-        suspend_chains = {f"{i}a": [[f"{i}a"]] for i in minor.nodes}
-        # number_of_layers = n // num_vertices_of_minor
-        number_of_layers = 1
-        G = {}
-        for i in range(number_of_layers):
+        build(
+            G,
+            remaining_chains,
+            next_start,
+            next_end,
+            {next_start},
+            next_available,
+            minor,
+            suspend_chains,
+        )
+        return
+
+    # Process current chain
+    for vertex in available_vertices:
+        newG = copy.deepcopy(G)
+        newG[current].add(vertex)
+        newG[vertex].add(current)
+
+        color_to_look_for = (
+            color(current) if color(vertex) == color(end) else color(end)
+        )
+        next_available = [
+            v
+            for v in G.keys()
+            if v not in visited.union({vertex}) and color(v) == color_to_look_for
+        ]
+
+        build(
+            newG,
+            chains_to_build,
+            vertex,
+            end,
+            visited.union({vertex}),
+            next_available,
+            minor,
+            suspend_chains,
+        )
+
+def generateColorings(H, n, num_vertices_of_minor):
+    number_of_layers = 1
+    G = {}
+    for i in range(number_of_layers):
             suffix = chr(ord("a") + i)
             for key in H:
                 new_key = key[:-1] + suffix
                 G[new_key] = set()
 
-        colorings = []
-        number_of_colors_left = n - num_vertices_of_minor
+    colorings = []
+    number_of_colors_left = n - num_vertices_of_minor
 
-        if number_of_colors_left != 0:
+    if number_of_colors_left != 0:
             all_combs = list(
                 combinations_with_replacement(H.keys(), number_of_colors_left)
             )
 
-            for comb in all_combs:
+    for comb in all_combs:
                 new_g = copy.deepcopy(G)
                 for value, group in itertools.groupby(comb):
                     current_layer = number_of_layers
@@ -421,10 +374,26 @@ if __name__ == "__main__":
                 print(new_g)
                 print(comb)
                 colorings.append(new_g)
-        else:
+    else:
             colorings.append(G)
 
-        print(len(colorings))
+    print(len(colorings))
+    return colorings
+
+def main(n, num_vertices_of_minor, minor_graphs):
+    index = 0
+
+    for minor in minor_graphs:
+        # builidng the graph H in the format suitable for the minorminer
+        H = {
+            f"{node}a": set([f"{neighbor}a" for neighbor in neighbors])
+            for node, neighbors in minor.adj.items()
+        }
+
+        small = get_edges_from_adjacency_list(H)
+        suspend_chains = {f"{i}a": [[f"{i}a"]] for i in minor.nodes}
+
+        colorings = generateColorings(H, n, num_vertices_of_minor)
 
         edges = {
             (min(node, neighbor), max(node, neighbor))
@@ -435,8 +404,7 @@ if __name__ == "__main__":
 
         coloring_index = 0
         for colroing in colorings:
-            print("Coloring:", coloring_index, index, len(graphs))
-            # random.shuffle(edges)  # Shuffle edge order for randomness
+            print("Coloring:", coloring_index, index, len(minor_graphs))
             color_to_look_for = color(edges[0][1])
             available_vertices = [
                 v for v in colroing.keys() if color(v) == color_to_look_for
@@ -453,14 +421,33 @@ if __name__ == "__main__":
             )
             coloring_index += 1
         index += 1
-        print("NEXT STEP", index, len(graphs))
+        print("NEXT STEP", index, len(minor_graphs))
 
-    if noMinorMinimalGraph is not None and noMinorGraph is not None:
-        print(noMinorGraph.edges())
-        print(noMinorMinimalGraph)
-        showGraph(noMinorGraph, "The minor we couldn't find")
-        showGraph(getNetworkxGraph(noMinorMinimalGraph), "The minimal counter example")
+if __name__ == "__main__":
+    #### Uncomment each part to run the specific test case
+    #### ------------------------------------------------------------####
+    #### 1. the counterexample from Kriesell's and Mohr's paper for K_7.
+    # minors = [
+    #     nx.Graph(
+    #         [
+    #             ("0", "1"),
+    #             ("1", "2"),
+    #             ("2", "3"),
+    #             ("3", "4"),
+    #             ("4", "5"),
+    #             ("5", "0"),
+    #             ("0", "6"),
+    #             ("6", "3"),
+    #         ]
+    #     )
+    # ]
+    # main(14, 7, minors) - this will find the Z(H) and it's supergraphs. 
+    #### ------------------------------------------------------------####
 
-    for m in nominors:
-        showGraph(m, "The minor we couldn't find")
-    print("STATISTICS", foundedMinor, noMinor)
+    #### 2. Checking for all spanning subgraphs of K_6 with supergraphs with 13 vertices - will take around 5-6 hours, for smaller graphs i.e shorter time run `main(12, 6, minors)`.
+    minors = get_graphs_from_geng(6)
+    main(13, 6, minors)
+    #### ------------------------------------------------------------####
+
+
+
